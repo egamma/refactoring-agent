1d27c43a37fe6587122712925c2e118dd38aae72
egamma <egamma@microsoft.com>
2024-01-06 14:44:40 +0100

    Fix issue that the TextContentProvider was not informed about the change of contents

--- src/extension.ts
+++ src/extension.ts
@@ -54,6 +54,8 @@ class RefactoringPreviewContentProvider implements vscode.TextDocumentContentPro
 	private originalContent: string = '';
 	private refactoredContent: string = '';
 	private fileExtension: string = '';
+	private _onDidChange = new vscode.EventEmitter<vscode.Uri>();
+    public readonly onDidChange = this._onDidChange.event;
 
 	updateContent(original: string, refactored: string, fileExtension: string) {
 		this.originalContent = original;
@@ -69,6 +71,10 @@ class RefactoringPreviewContentProvider implements vscode.TextDocumentContentPro
 		}
 		return `Failed to provide content for the given uri ${uri}`;
 	}
+
+    public update(uri: vscode.Uri) {
+        this._onDidChange.fire(uri);
+    }
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -461,6 +467,8 @@ export function activate(context: vscode.ExtensionContext) {
 			const refactoredUri = vscode.Uri.parse(`refactoring-preview:refactored${fileExtension}`);
 
 			previewContentProvider.updateContent(arg.originalCode, refactoredCode, fileExtension);
+			previewContentProvider.update(originalUri);
+			previewContentProvider.update(refactoredUri);
 			vscode.commands.executeCommand('vscode.diff', originalUri, refactoredUri);
 
 			// annotate the URI with a query parameter that contains the refactoring target
